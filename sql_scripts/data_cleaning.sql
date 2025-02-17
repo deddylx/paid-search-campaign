4dddf6b0cf61189f252d2b3706b984f941e02562
deddylx <158553254+deddylx@users.noreply.github.com>
2025-02-17 16:21:05 +0800

    Update data_cleaning.sql

--- sql_scripts/data_cleaning.sql
+++ sql_scripts/data_cleaning.sql
@@ -1,4 +1,4 @@
--- create table
+-- 1. CREATE TABLE
 create table mall_campaigns (
 	ad_group varchar(50),
 	month varchar(10),
@@ -18,40 +18,45 @@ create table mall_campaigns (
 select * from mall_campaigns;
 
 
--- finding duplicate rows
-SELECT (mall_campaigns.*)::text, count(*)
-FROM mall_campaigns
-GROUP BY mall_campaigns.*
-HAVING count(*) > 1
+-- 2. FIND DUPLICATE ROWS
+select
+    (mall_campaigns.*)::text, 
+    count(*)
+from mall_campaigns
+group by mall_campaigns.*
+having count(*) > 1
 -- result: no duplicate rows
 
--- check missing values
-SELECT
-    attname AS column_name,
-    null_frac AS null_fraction
-FROM pg_stats
-WHERE tablename = 'mall_campaigns';
+	
+-- 3. CHECK MISSING VALUES
+select
+    attname as column_name,
+    null_frac as null_fraction
+from pg_stats
+where tablename = 'mall_campaigns';
 -- result: no missing values
 
--- recalculate metrics
--- ctr
+
+-- 4. RECALCULATE METRICS
+--- ctr
 update mall_campaigns
 set ctr = clicks / impressions::numeric;
 
--- conv_rate
+--- conv_rate
 update mall_campaigns
 set conv_rate = conversions / clicks::numeric;
 
--- cpc
+--- cpc
 update mall_campaigns
 set cpc = cost / clicks::numeric;
 
--- pnl
+--- pnl
 update mall_campaigns
 set pnl = revenue - cost;
 
 
--- create new columns (roas)
+--- 5. CREATE NEW COLUMNS
+--- roas
 alter table mall_campaigns
 add column roas numeric;
 
@@ -59,14 +64,14 @@ update mall_campaigns
 set roas = revenue / cost::numeric;
 
 
--- add new columns (for transformed data)
+-- 6. ADD NEW COLUMNS
 alter table mall_campaigns
 add column match_type varchar(10),
 add column device_type varchar(10),
 add column ad_type varchar(50),
 add column date date;
 
--- transform data
+--- transform data
 update mall_campaigns
 set 
 	match_type = split_part(ad_group, ' - ', 1),
@@ -74,5 +79,5 @@ set
 	ad_type = split_part(ad_group, ' - ', 3),
 	date = to_date(month || '2021', 'Month YYYY');,
 
--- check the result
+-- 7. CHECK THE RESULTS
 select * from mall_campaigns;
